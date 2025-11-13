fd173ad7e986bd2ae4b2b91ddc2f40548b86018b
pavinair14 <pavinair14@gmail.com>
2025-11-13 22:56:11 +0530

    updating app title

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
   const { t } = useTranslation();
   return (
     <ErrorBoundary>
-      <div className="min-h-screen h-full flex flex-col items-center bg-gradient-to-b from-violet-200 to-violet-100 pt-8 pb-0 sm:pb-16">
+      <div className="min-h-screen h-full flex flex-col items-center bg-stone-100 pt-8 pb-0 sm:pb-16">
         <a href="#main-content" className="sr-only focus:not-sr-only focus:fixed focus:top-4 focus:left-4 bg-white text-violet-900 p-2 rounded">
           {t('messages.skipToMainContent')}
         </a>
@@ -20,7 +20,7 @@ function App() {
         {/* form */}
         <main
           id="main-content"
-          className="z-10 w-full h-full max-w-4xl px-10 py-6 bg-white rounded-xl shadow-lg"
+          className="z-10 w-full h-full max-w-4xl px-10 py-6 bg-white rounded-xl shadow-xl"
         >
           <MultiStepForm />
         </main>
